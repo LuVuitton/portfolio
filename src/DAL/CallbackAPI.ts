d4831efbc334ac9b193ac54b949abdc68d157c39
FelixT <felixtekhada@gmail.com>
2023-05-08 23:02:00 +0200

    I have changed the local request to Vercel

--- src/DAL/CallbackAPI.ts
+++ src/DAL/CallbackAPI.ts
@@ -6,7 +6,7 @@ import axios from "axios"
 export const callbackAPI = {
 
     sendDataForFeedback(data:any){
-        return axios.post('http://localhost:3010/sendMessage', data)
+        return axios.post('https://portfolio-back-6xiy-git-main-luvuitton.vercel.app/sendMessage', data)
             .then(()=> {
                 console.log('successfully')
             })
